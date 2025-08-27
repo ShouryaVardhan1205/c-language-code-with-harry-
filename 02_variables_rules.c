352ae8b530b807cd4f83ce9a0fdca39fad52fd76
ShouryaVardhan1205 <shouryavardhan1205@gmail.com>
2025-08-27 23:02:46 +0530

    Add files via upload

---
+++ 02_variables_rules.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+    int a;
+    a = 6;
+    
+    int b;
+    int c;
+    int cab;
+    int shourya;
+    int Shourya;
+    int Shourya_good;   
+    b = 4;
+    printf ("%d",a);
+    
+    printf ("%d",b);
+    return 0;
+}  
---
+++ 05_comments.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+/*
+Author: Shourya Vardhan 
+Date: 18.08.2025
+*/
+printf ("Hello World");
+//Return 0 is returning 0 to the OS. 
+    return 0;
+}
