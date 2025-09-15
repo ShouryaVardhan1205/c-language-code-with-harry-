1ae34a1eaabd034b193d3e2f91012e62a1c64898
ShouryaVardhan1205 <shouryavardhan1205@gmail.com>
2025-09-15 17:27:06 +0530

    Add files via upload

---
+++ ascii_value.c
@@ -0,0 +1,10 @@
+//How to get ascii value of a character.
+#include<stdio.h>
+    int main()
+    {
+    char ch;
+    printf("Enter character:\n");
+    scanf("%c", &ch);
+    printf("The ascii value of the character is %d", ch);
+        return 0;
+    }
